5defbe8db1f77c5e6f708af05c4b51f67b944f56
Toon Van den Bos <toon@whitecube.be>
2021-04-23 11:47:05 +0200

    Added base homepage content injection

--- wp-content/themes/test/index.php
+++ wp-content/themes/test/index.php
@@ -6,6 +6,17 @@
     <title><?php is_front_page() ? bloginfo('description') : wp_title('Mon site •'); ?></title>
 </head>
 <body>
-    <h1>Hello world !</h1>
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <header class="header">
+            <h1 class="header__title"><?php the_title(); ?></h1>
+        </header>
+        <main class="content">
+            <div class="content__wysiwyg"><?php the_content(); ?></div>
+        </main>
+    <?php endwhile; else : ?>
+        <div class="empty">
+            <p class="empty__message">Oups, nous n'avons rien à afficher.</p>
+        </div>
+    <?php endif; ?>
 </body>
 </html>
