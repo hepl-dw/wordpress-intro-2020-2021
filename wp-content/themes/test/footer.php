4eb9c7a855dce80266736dedf072893057dd0b3a
Toon Van den Bos <toon@whitecube.be>
2021-05-07 10:07:16 +0200

    Added footer menu

--- wp-content/themes/test/footer.php
+++ wp-content/themes/test/footer.php
@@ -1,5 +1,12 @@
     <footer class="end">
         <p class="end__copyright">© Toon Van den Bos</p>
+        <nav class="end__menu menu">
+            <h2 class="sro">Liens utiles</h2>
+
+            <?php foreach(dw_menu('footer') as $link): ?>
+            <a href="<?= $link->url; ?>" class="menu__link"><?= $link->label; ?></a>
+            <?php endforeach; ?>
+        </nav>
     </footer>
     <?php wp_footer(); ?>
 </body>
